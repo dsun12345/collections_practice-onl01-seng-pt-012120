a1e6d68a471a17067595363cbf218fafc796eed6
Daniel Sun <danielsun12345@gmail.com>
2020-02-01 03:38:34 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,34 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort do |a,b|
+    if a == b 
+      0 
+    elsif a < b 
+     1
+    elsif a > b 
+     -1
+   end 
+ end 
+end
+
+def sort_array_char_count(array)
+  array.sort {|a,b| a.size <=> b.size}
+end 
+
+def swap_elements(array)
+  array[0], array[1],array[2] =array[0],array[2],array[1]
+end 
+
+def reverse_array(array)
+ array.reverse
+end
+
+def kesha_maker(array)
+  arrays=[]
+  array.each do |x| arrays << x.charsat(3) = "$"
+end 
+arrays
+end
